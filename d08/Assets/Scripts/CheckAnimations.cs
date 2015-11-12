6cc91de5c3cfd1ff80fcaf69297fca36e497619c
Michael YADABA <fyadaba@e1r7p6.42.fr>
2015-11-12 23:03:13 +0100

    Animated Maya.

---
+++ d08/Assets/Scripts/CheckAnimations.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckAnimations : MonoBehaviour {
+
+	private Animator	anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("r")) {
+			anim.SetBool ("running", true);
+		} else {
+			anim.SetBool("running", false);
+		}
+		if (Input.GetKeyDown ("a")) {
+			anim.SetTrigger ("attack");
+			anim.SetBool ("attacking", true);
+		} else if (Input.GetKeyDown ("s")) {
+			anim.SetBool ("attacking", false);
+		}
+		if (Input.GetKeyDown ("d")) {
+			anim.SetTrigger("death");
+			anim.SetBool ("dead", true);
+		}
+	}
+}
