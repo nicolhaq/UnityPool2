3e5310835164b2feedf751ab461918aff066cac4
Nicolas HAQUET <nhaquet@e1r8p5.42.fr>
2015-11-12 22:26:53 +0100

    ok

---
+++ d08/Assets/move.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+	public float distance  = 4.5f;
+	public GameObject map;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		  if (Input.GetMouseButton(0))
+		 	GetComponent<NavMeshAgent>().destination = CastRayToWorld();
+	}
+	 Vector3 CastRayToWorld() {
+     	Ray ray  = Camera.main.ScreenPointToRay(Input.mousePosition);    
+    	 Vector3 point = ray.origin + (ray.direction * distance);    
+    	 Debug.Log( "World point " + point );
+    	 return(point);
+ 	}	
+}
