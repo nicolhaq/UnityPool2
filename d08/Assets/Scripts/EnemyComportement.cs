65ff5d22f539073e5ad15bd86f02d703728ae526
Jean SPEZIA <jspezia@e3r11p23.42.fr>
2015-11-13 21:45:20 +0100

    p

--- d08/Assets/Scripts/EnemyComportement.cs
+++ d08/Assets/Scripts/EnemyComportement.cs
@@ -3,18 +3,12 @@ using System.Collections;
 
 public class EnemyComportement : MonoBehaviour {
 
-	public int hp;
-	public GameObject maya;
 	private Animator	anim;
 
-	// Use this for initialization
 	void Start () {
-		hp = 3;
 		anim = GetComponent<Animator>();
 	}
 
-	
-	// Update is called once per frame
 	void Update () {
 		if (GetComponent<Stat>().HP == 0 && !anim.GetBool("dead")) {
 			anim.SetTrigger("death");
---
+++ d08/Assets/Scripts/UpdateSlider.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UpdateSlider : MonoBehaviour {
+
+	public Slider		HpSlider;
+	public Slider		XpSlider;
+
+	void Update () {
+		HpSlider.GetComponent<Slider>().value = GetComponent(Stat).HP;
+		XpSlider.GetComponent<Slider>().value = GetComponent(Stat).XP;
+	}
+}
